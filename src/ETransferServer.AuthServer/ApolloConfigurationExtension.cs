3c438f5d1fc4975118d226a2c283bf18cea8609c
zltrsf <zltrsf@etransfer.exchange>
2024-04-17 16:17:12 +0800

    feat: auth add extension

---
+++ src/ETransferServer.AuthServer/ApolloConfigurationExtension.cs
@@ -0,0 +1,10 @@
+namespace ETransferServer.Auth;
+
+public static class ApolloConfigurationExtension
+{
+    public static IHostBuilder UseApollo(this IHostBuilder builder)
+    {
+        return builder
+            .ConfigureAppConfiguration(config => { config.AddApollo(config.Build().GetSection("apollo")); });
+    }
+}
