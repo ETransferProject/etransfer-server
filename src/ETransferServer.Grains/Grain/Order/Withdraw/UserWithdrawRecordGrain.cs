8d27a4956ec58a8f1896c753b70fa0b8bf236bd4
Henry-002 <henry@etransfer.exchange>
2024-05-09 16:05:32 +0800

    fix: order value

--- src/ETransferServer.Grains/Grain/Order/Withdraw/UserWithdrawRecordGrain.cs
+++ src/ETransferServer.Grains/Grain/Order/Withdraw/UserWithdrawRecordGrain.cs
@@ -15,8 +15,7 @@ namespace ETransferServer.Grains.Grain.Order.Withdraw;
 public interface IUserWithdrawRecordGrain : IGrainWithGuidKey
 {
     Task<CommonResponseDto<WithdrawOrderDto>> AddOrUpdateAsync(WithdrawOrderDto orderDto);
-
-
+    
     Task<CommonResponseDto<WithdrawOrderDto>> GetAsync();
 }
 
@@ -45,13 +44,21 @@ public class UserWithdrawRecordGrain : Grain<WithdrawOrderState>, IUserWithdrawR
     {
         try
         {
+            (var status, var lastModifyTime, var isReserved) = 
+                (State.Status, State.LastModifyTime, IsReservedInAElf(orderDto));
             _objectMapper.Map(orderDto, State);
             var now = DateTime.UtcNow.ToUtcMilliSeconds();
             var createTime = State.CreateTime ?? DateTime.UtcNow.ToUtcMilliSeconds();
-
             State.Id = this.GetPrimaryKey();
             State.CreateTime = createTime;
             State.LastModifyTime = now;
+            if (isReserved)
+            {
+                _logger.LogDebug("Save withdraw order, orderId={OrderId}, status={Status}, lastModifyTime={LastModifyTime}", 
+                    orderDto.Id, status, lastModifyTime);
+                State.Status = status;
+                State.LastModifyTime = lastModifyTime;
+            }
             if (!State.ArrivalTime.HasValue)
             {
                 var netWorkInfo = _withdrawNetworkOptions.Value.NetworkInfos.FirstOrDefault(t =>
@@ -95,6 +102,38 @@ public class UserWithdrawRecordGrain : Grain<WithdrawOrderState>, IUserWithdrawR
             ? new Task<CommonResponseDto<WithdrawOrderDto>>(null!)
             : Task.FromResult(
                 new CommonResponseDto<WithdrawOrderDto>(_objectMapper.Map<WithdrawOrderState, WithdrawOrderDto>(State)));
+    }
+
+    private bool IsReservedInAElf(WithdrawOrderDto orderDto)
+    {
+        var isAElf = orderDto.ToTransfer.Network == CommonConstant.Network.AElf;
+        var fromStatus = State.FromTransfer.Status == OrderTransferStatusEnum.Transferred.ToString()
+                         && (State.Status == OrderStatusEnum.FromTransferConfirmed.ToString()
+                             || State.Status == OrderStatusEnum.ToStartTransfer.ToString()
+                             || State.Status == OrderStatusEnum.ToTransferring.ToString()
+                             || State.Status == OrderStatusEnum.ToTransferred.ToString()
+                             || State.Status == OrderStatusEnum.ToTransferConfirmed.ToString()
+                             || State.Status == OrderStatusEnum.Finish.ToString())
+                         && (orderDto.FromTransfer.Status == OrderTransferStatusEnum.Transferred.ToString()
+                             || orderDto.FromTransfer.Status == OrderTransferStatusEnum.Confirmed.ToString())
+                         && (orderDto.Status == OrderStatusEnum.FromTransferred.ToString()
+                             || orderDto.Status == OrderStatusEnum.FromTransferConfirmed.ToString());
+
+        var toStatus = State.ToTransfer.Status == OrderTransferStatusEnum.Transferred.ToString()
+                       && State.Status == OrderStatusEnum.ToTransferConfirmed.ToString()
+                       && (orderDto.ToTransfer.Status == OrderTransferStatusEnum.Transferred.ToString()
+                           || orderDto.ToTransfer.Status == OrderTransferStatusEnum.Confirmed.ToString())
+                       && (orderDto.Status == OrderStatusEnum.ToTransferred.ToString()
+                           || orderDto.Status == OrderStatusEnum.ToTransferConfirmed.ToString());
+        
+        var finishStatus = State.ToTransfer.Status == OrderTransferStatusEnum.Transferred.ToString()
+                       && State.Status == OrderStatusEnum.Finish.ToString()
+                       && (orderDto.ToTransfer.Status == OrderTransferStatusEnum.Transferred.ToString()
+                           || orderDto.ToTransfer.Status == OrderTransferStatusEnum.Confirmed.ToString())
+                       && (orderDto.Status == OrderStatusEnum.ToTransferred.ToString()
+                           || orderDto.Status == OrderStatusEnum.ToTransferConfirmed.ToString()
+                           || orderDto.Status == OrderStatusEnum.Finish.ToString());
+        return isAElf && (fromStatus || toStatus || finishStatus);
     }
 
     private bool? IsBigAmountInAElf(WithdrawOrderDto orderDto)
