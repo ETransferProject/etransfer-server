a1dca7ca852218bf0c1221a6928cb6587c4240ec
james_box <james_box@etransfer.exchange>
2024-05-11 15:01:00 +0800

    Fix: fix message

--- src/ETransferServer.Application/Order/OrderDepositAppService.cs
+++ src/ETransferServer.Application/Order/OrderDepositAppService.cs
@@ -178,7 +178,7 @@ public class OrderDepositAppService : ApplicationService, IOrderDepositAppServic
         AssertHelper.IsTrue(_networkInfoOptions.Value.NetworkMap.ContainsKey(request.ToSymbol), 
             "ToSymbol is not exist. Please refresh and try again.");
         AssertHelper.IsTrue(DepositSwapAmountHelper.IsValidRange(request.FromAmount), "Param [FromAmount] is invalid. Please refresh and try again.");
-        AssertHelper.IsTrue(_tokenAppService.IsValidSwap(request.ToChainId, request.FromSymbol, request.ToSymbol), "Must be a valid Swap Deposit. Please refresh and try again.");
+        AssertHelper.IsTrue(_tokenAppService.IsValidSwap(request.ToChainId, request.FromSymbol, request.ToSymbol), "Must be valid Deposit Symbol. Please refresh and try again.");
 
         return new CalculateDepositRateDto()
         {
