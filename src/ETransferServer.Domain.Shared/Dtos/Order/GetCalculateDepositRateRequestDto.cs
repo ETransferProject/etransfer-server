f4853856c175f95e1223831927d313f5fe7e981b
james_box <james_box@etransfer.exchange>
2024-05-15 18:43:16 +0800

    Feat: check FromAmount

--- src/ETransferServer.Domain.Shared/Dtos/Order/GetCalculateDepositRateRequestDto.cs
+++ src/ETransferServer.Domain.Shared/Dtos/Order/GetCalculateDepositRateRequestDto.cs
@@ -1,10 +1,25 @@
 
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace ETransferServer.Models;
 
-public class GetCalculateDepositRateRequestDto
+public class GetCalculateDepositRateRequestDto : IValidatableObject
 {
     public string ToChainId { get; set; }
     public string FromSymbol { get; set; }
     public string ToSymbol { get; set; }
     public decimal FromAmount { get; set; }
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        int decimalPlaces = FromAmount.ToString().Length - FromAmount.ToString().IndexOf('.') - 1;
+        if (decimalPlaces > 8 || FromAmount < 0)
+        {
+            yield return new ValidationResult(
+                "Amount invalid.",
+                new[] { "Amount" }
+            );
+        }
+    }
 }
