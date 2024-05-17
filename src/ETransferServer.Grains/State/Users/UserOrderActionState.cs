a7052a8cd9f0e744f53e128d7d8b5be0d52b44b2
james_box <james_box@etransfer.exchange>
2024-05-17 14:26:40 +0800

    fix: add UserId for UserOrderActionState

--- src/ETransferServer.Grains/State/Users/UserOrderActionState.cs
+++ src/ETransferServer.Grains/State/Users/UserOrderActionState.cs
@@ -5,6 +5,7 @@ namespace ETransferServer.Grains.State.Users;
 public class UserOrderActionState
 {
     public Guid Id { get; set; }
+    public string UserId { get; set; }
     public long LastModifyTime { get; set; }
 }
 
