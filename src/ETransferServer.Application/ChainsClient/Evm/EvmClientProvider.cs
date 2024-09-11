19189a38445f9887f60d4aa70f4ae28a71b3263a
Henry-002 <henry@etransfer.exchange>
2024-09-11 10:53:48 +0800

    fix: add evm timeout

--- src/ETransferServer.Application/ChainsClient/Evm/EvmClientProvider.cs
+++ src/ETransferServer.Application/ChainsClient/Evm/EvmClientProvider.cs
@@ -1,19 +1,27 @@
+using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using ETransferServer.Common;
 using ETransferServer.Common.ChainsClient;
+using ETransferServer.Options;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace ETransferServer.ChainsClient.Evm;
 
 public class EvmClientProvider : IBlockchainClientProvider
 {
     protected readonly IBlockchainClientFactory<Nethereum.Web3.Web3> BlockchainClientFactory;
+    private readonly BlockChainInfoOptions _blockChainInfoOptions;
     private readonly ILogger<EvmClientProvider> _logger;
 
-    public EvmClientProvider(IBlockchainClientFactory<Nethereum.Web3.Web3> blockchainClientFactory, ILogger<EvmClientProvider> logger)
+    public EvmClientProvider(IBlockchainClientFactory<Nethereum.Web3.Web3> blockchainClientFactory, 
+        IOptionsSnapshot<BlockChainInfoOptions> apiOptions,
+        ILogger<EvmClientProvider> logger)
     {
         BlockchainClientFactory = blockchainClientFactory;
+        _blockChainInfoOptions = apiOptions.Value;
         _logger = logger;
     }
 
@@ -22,14 +30,26 @@ public class EvmClientProvider : IBlockchainClientProvider
     public async Task<BlockDtos> GetBlockTimeAsync(string chainId, string blockHash,string txId = null)
     {
         var client = BlockchainClientFactory.GetClient(chainId);
-        var block = await client.Eth.Blocks.GetBlockWithTransactionsByHash.SendRequestAsync(blockHash);
-        _logger.LogInformation("Get time from block.{chainId},{blockHash},{time}",chainId,block.BlockHash,block.Timestamp.Value);
-        return new BlockDtos
+        using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(_blockChainInfoOptions.TimeOut)))
         {
-            BlockHash = block.BlockHash,
-            BlockTimeStamp = block.Timestamp.Value,
-            BlockHeight = block.Number.Value,
-            TransactionIdList = block.Transactions.ToList().Select(t => t.TransactionHash).ToList()
-        };
+            try
+            {
+                _logger.LogInformation("Evm start, timeOut:{timeOut}", _blockChainInfoOptions.TimeOut);
+                var block = await client.Eth.Blocks.GetBlockWithTransactionsByHash.SendRequestAsync(blockHash, cts.Token);
+                _logger.LogInformation("Get time from block.{chainId},{blockHash},{time}",chainId,block.BlockHash,block.Timestamp.Value);
+                return new BlockDtos
+                {
+                    BlockHash = block.BlockHash,
+                    BlockTimeStamp = block.Timestamp.Value,
+                    BlockHeight = block.Number.Value,
+                    TransactionIdList = block.Transactions.ToList().Select(t => t.TransactionHash).ToList()
+                };
+            }
+            catch (TaskCanceledException)
+            {
+                _logger.LogInformation("Evm cancel");
+            }
+        }
+        return new BlockDtos();
     }
 }
