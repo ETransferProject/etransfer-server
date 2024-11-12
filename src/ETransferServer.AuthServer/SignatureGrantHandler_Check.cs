f4d3a9e50fe940fcf752fe6d0101f3c5469a97f3
Henry-002 <henry@etransfer.exchange>
2024-11-12 14:05:11 +0800

    feat: add trx signature check

--- src/ETransferServer.AuthServer/SignatureGrantHandler_Check.cs
+++ src/ETransferServer.AuthServer/SignatureGrantHandler_Check.cs
@@ -3,8 +3,10 @@ using AElf;
 using AElf.ExceptionHandler;
 using ETransferServer.Auth.Options;
 using ETransferServer.Common;
+using NBitcoin.DataEncoders;
 using Nethereum.Hex.HexConvertors.Extensions;
 using Nethereum.Signer;
+using Nethereum.Util;
 using Solnet.Wallet;
 
 namespace ETransferServer.Auth;
@@ -38,7 +40,18 @@ public partial class SignatureGrantHandler
                         var pubKey = new PublicKey(addressRaw);
                         return pubKey.Verify(messageByte, signature);
                     case WalletEnum.TRX:
-                        return true;
+                        var fullMessage = String.Concat("\x19TRON Signed Message:\n", message.Length, message);
+                        var messageHash = new Sha3Keccack().CalculateHash(Encoding.UTF8.GetBytes(fullMessage));
+                        if (signature.Length != 65)
+                            throw new ArgumentException("Invalid signature length");
+                        var r = new Span<byte>(signature, 0, 32).ToArray();
+                        var s = new Span<byte>(signature, 32, 32).ToArray();
+                        var v = signature[64];
+                        if (v < 27) v += 27;
+                        var ecKey = EthECKey.RecoverFromSignature(EthECDSASignatureFactory.FromComponents(r, s, v), messageHash);
+                        var userAddress = ecKey.GetPublicAddress();
+                        var trxAddress = new Base58CheckEncoder().DecodeData(addressRaw);
+                        return userAddress.ToLowerInvariant().EndsWith(BitConverter.ToString(trxAddress, 1).Replace("-", "").ToLowerInvariant());
                     case WalletEnum.TON:
                         return true;
                     default:
