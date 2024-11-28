f4674037838b45b69342f420d24ec3de9158805e
Henry-002 <henry@etransfer.exchange>
2024-11-28 16:19:06 +0800

    fix: modify hub address

--- src/ETransferServer.HttpApi/Hubs/OrderChangeHandler.cs
+++ src/ETransferServer.HttpApi/Hubs/OrderChangeHandler.cs
@@ -11,7 +11,6 @@ using MassTransit;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Orleans;
 using Volo.Abp.DependencyInjection;
 
 namespace ETransferServer.Hubs
@@ -22,21 +21,18 @@ namespace ETransferServer.Hubs
         private readonly IHubConnectionProvider _hubConnectionProvider;
         private readonly IUserAppService _userAppService;
         private readonly IOrderAppService _orderAppService;
-        private readonly IClusterClient _clusterClient;
         private readonly ILogger<OrderChangeHandler> _logger;
         
         public OrderChangeHandler(IHubConnectionProvider hubConnectionProvider,
             IHubContext<EtransferHub> hubContext,
             IUserAppService userAppService,
             IOrderAppService orderAppService,
-            IClusterClient clusterClient,
             ILogger<OrderChangeHandler> logger)
         {
             _hubConnectionProvider = hubConnectionProvider;
             _hubContext = hubContext;
             _userAppService = userAppService;
             _orderAppService = orderAppService;
-            _clusterClient = clusterClient;
             _logger = logger;
         }
 
@@ -52,7 +48,6 @@ namespace ETransferServer.Hubs
                 string.Join(CommonConstant.Comma, connectionIds));
             var result = await _orderAppService.GetUserOrderRecordListAsync(new GetUserOrderRecordRequestDto
             {
-                Address = addressList.Count == 3 ? addressList[0] : null,
                 AddressList = userAddressList,
                 Time = 0
             }, eventData.Message);
@@ -72,30 +67,11 @@ namespace ETransferServer.Hubs
             var userDto = await _userAppService.GetUserByIdAsync(eto.UserId.ToString());
             var address = userDto?.AddressInfos?.FirstOrDefault()?.Address;
             var addressList = address.IsNullOrEmpty() ? new List<string>() : new List<string> { address };
-            addressList.Add(await GetFullAddressAsync(eto.FromTransfer.Network, eto.FromTransfer.FromAddress));
-            addressList.Add(await GetFullAddressAsync(eto.ToTransfer.Network, eto.ToTransfer.ToAddress));
 
             var userAddressList = address.IsNullOrEmpty()
                 ? new List<GetUserAddressDto>()
                 : new List<GetUserAddressDto> { new() { SourceType = userDto?.AppId, Address = address }};
-            userAddressList.Add(new() { SourceType = await GetWalletTypeAsync(eto.FromTransfer.Network) ?? WalletEnum.Portkey.ToString(), Address = eto.FromTransfer.FromAddress });
-            userAddressList.Add(new() { SourceType = await GetWalletTypeAsync(eto.ToTransfer.Network) ?? WalletEnum.Portkey.ToString(), Address = eto.ToTransfer.ToAddress });
             return Tuple.Create(addressList, userAddressList);
-        }
-        
-        private async Task<string> GetFullAddressAsync(string network, string address)
-        {
-            var walletType = await GetWalletTypeAsync(network);
-            if (walletType.IsNullOrEmpty()) return address;
-            return string.Concat(walletType, CommonConstant.Underline, address);
-        }
-
-        private async Task<string> GetWalletTypeAsync(string network)
-        {
-            if (network == ChainId.AELF) return null;
-            if (Enum.TryParse<WalletEnum>(network, true, out var walletType) 
-                && (int)walletType > 2) return network.ToLower();
-            return WalletEnum.EVM.ToString().ToLower();
         }
     }
 }
